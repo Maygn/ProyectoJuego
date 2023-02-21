b92f659a0c6082e15556e17e08bdb1eb378ac50f
Maygn <Mayneyra87@gmail.com>
2023-02-21 18:05:31 +0100

    Probando

--- movimientos/Realizable.java
+++
@@ -1,9 +0,0 @@
-package com.example.models.movimientos;
-
-import com.example.models.Personaje;
-
-public interface Realizable {
-    
-    public void realizar(Personaje activo, Personaje pasivo);
-
-}
