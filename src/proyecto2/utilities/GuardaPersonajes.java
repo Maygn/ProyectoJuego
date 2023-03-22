348e731f0dc7e6d761338486379756729395bc4e
Maygn <Mayneyra87@gmail.com>
2023-03-22 19:31:40 +0100

    Cole 22/3
    Ignora los .class, dan igual. Mira este y dime si esta bie
    si, ese es el que he hecho ahora, solo deberia pushear ese

--- src/proyecto2/utilities/GuardaPersonajes.java
+++ src/proyecto2/utilities/GuardaPersonajes.java
@@ -13,7 +13,13 @@ import java.util.ArrayList;
 import proyecto2.models.Personaje;
 
 public class GuardaPersonajes {
-
+/**
+ * @param Personaje p
+ * @param String destino
+ * Primero genera un archivo en la carpeta indicada (Destino) llamado personaje.pj (la extension
+ * no sirve para nada, pero queda chula). El false indica que no añada el contenido al final, sino
+ * que sobreescriba.
+ * Después mete el contenido del personaje dentro del archivo que ha creado.*/
 	public static void guardar(Personaje p, String destino) throws IOException {
 		FileOutputStream guardaTexto = new FileOutputStream(new File(destino + "/personaje.pj"), false);
 		ObjectOutputStream guardaObjetos = new ObjectOutputStream(guardaTexto);
@@ -21,7 +27,13 @@ public class GuardaPersonajes {
 		guardaObjetos.writeObject(p);
 		guardaObjetos.close();
 	}
-
+/**
+ * @param Personaje p
+ * @param String destino
+ * Funciona exactamente igual que el anterior pero automatizamos que les ponga diferente nombre a los
+ * archivos. Esto es porque solo queremos un personaje jugable (si siempre le pone el mismo nombre al
+ * archivo no va a poder crear varios) pero en el caso de pnj necesito poner un archivo para cada uno 
+ * en la misma carpeta*/
 	public static void guardarPNJ(Personaje p, String destino) throws IOException {
 		FileOutputStream guardaTexto = new FileOutputStream(new File(destino + "/" + p.getNombre()), false);
 		ObjectOutputStream guardaObjetos = new ObjectOutputStream(guardaTexto);
@@ -29,7 +41,11 @@ public class GuardaPersonajes {
 		guardaObjetos.writeObject(p);
 		guardaObjetos.close();
 	}
-
+/**
+ * @param String destino
+ * @return personaje pr1
+ * coge el archivo del personaje jugable de la carpeta y lee el personaje, luego returna el
+ * personaje como objeto*/
 	public static Personaje cargar(String destino) throws ClassNotFoundException, IOException {
 		FileInputStream leeTexto = new FileInputStream(new File(destino + "/personaje.pj"));
 		ObjectInputStream leeObjetos = new ObjectInputStream(leeTexto);
@@ -41,7 +57,11 @@ public class GuardaPersonajes {
 
 		return pr1;
 	}
-
+/**
+ * @param String destino
+ * @return ArrayList<Personaje> npcs
+ * Coge uno por uno los archivos de los npc que están en la carpeta de los npc, lee los objetos
+ * y crea un arraylist de objetos personaje con todos los npc */
 	public static ArrayList<Personaje> cargarPNJS(String destino) throws ClassNotFoundException, IOException {
 		
 		File carpetaPnjs=new File("baulNPC");
